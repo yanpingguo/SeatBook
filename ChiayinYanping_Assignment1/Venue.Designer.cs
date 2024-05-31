8352f3467d6212b448066b3b09e95dc5c399907e
Chiayin <Chiayin@DESKTOP-IV3LRLM>
2024-05-31 17:07:41 -0400

    Add project files.

---
+++ ChiayinYanping_Assignment1/Venue.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace ChiayinYanping_Assignment1
+{
+    partial class Venue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnC4 = new System.Windows.Forms.Button();
+            this.btnC3 = new System.Windows.Forms.Button();
+            this.btnB4 = new System.Windows.Forms.Button();
+            this.btnB3 = new System.Windows.Forms.Button();
+            this.btnC2 = new System.Windows.Forms.Button();
+            this.btnA4 = new System.Windows.Forms.Button();
+            this.btnB2 = new System.Windows.Forms.Button();
+            this.btnC1 = new System.Windows.Forms.Button();
+            this.btnA3 = new System.Windows.Forms.Button();
+            this.btnB1 = new System.Windows.Forms.Button();
+            this.btnA2 = new System.Windows.Forms.Button();
+            this.btnA1 = new System.Windows.Forms.Button();
+            this.lblTotalCapacity = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRows = new System.Windows.Forms.Label();
+            this.lblColums = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAddToWallet = new System.Windows.Forms.Button();
+            this.btnFillAllSeats = new System.Windows.Forms.Button();
+            this.btnCancelAllBookings = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnC4);
+            this.groupBox1.Controls.Add(this.btnC3);
+            this.groupBox1.Controls.Add(this.btnB4);
+            this.groupBox1.Controls.Add(this.btnB3);
+            this.groupBox1.Controls.Add(this.btnC2);
+            this.groupBox1.Controls.Add(this.btnA4);
+            this.groupBox1.Controls.Add(this.btnB2);
+            this.groupBox1.Controls.Add(this.btnC1);
+            this.groupBox1.Controls.Add(this.btnA3);
+            this.groupBox1.Controls.Add(this.btnB1);
+            this.groupBox1.Controls.Add(this.btnA2);
+            this.groupBox1.Controls.Add(this.btnA1);
+            this.groupBox1.Location = new System.Drawing.Point(80, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 362);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Venue:";
+            // 
+            // btnC4
+            // 
+            this.btnC4.Location = new System.Drawing.Point(337, 260);
+            this.btnC4.Name = "btnC4";
+            this.btnC4.Size = new System.Drawing.Size(66, 57);
+            this.btnC4.TabIndex = 0;
+            this.btnC4.Text = "C4";
+            this.btnC4.UseVisualStyleBackColor = true;
+            // 
+            // btnC3
+            // 
+            this.btnC3.Location = new System.Drawing.Point(233, 260);
+            this.btnC3.Name = "btnC3";
+            this.btnC3.Size = new System.Drawing.Size(66, 57);
+            this.btnC3.TabIndex = 0;
+            this.btnC3.Text = "C3";
+            this.btnC3.UseVisualStyleBackColor = true;
+            // 
+            // btnB4
+            // 
+            this.btnB4.Location = new System.Drawing.Point(337, 159);
+            this.btnB4.Name = "btnB4";
+            this.btnB4.Size = new System.Drawing.Size(66, 57);
+            this.btnB4.TabIndex = 0;
+            this.btnB4.Text = "B4";
+            this.btnB4.UseVisualStyleBackColor = true;
+            // 
+            // btnB3
+            // 
+            this.btnB3.Location = new System.Drawing.Point(233, 159);
+            this.btnB3.Name = "btnB3";
+            this.btnB3.Size = new System.Drawing.Size(66, 57);
+            this.btnB3.TabIndex = 0;
+            this.btnB3.Text = "B3";
+            this.btnB3.UseVisualStyleBackColor = true;
+            // 
+            // btnC2
+            // 
+            this.btnC2.Location = new System.Drawing.Point(139, 260);
+            this.btnC2.Name = "btnC2";
+            this.btnC2.Size = new System.Drawing.Size(66, 57);
+            this.btnC2.TabIndex = 0;
+            this.btnC2.Text = "C2";
+            this.btnC2.UseVisualStyleBackColor = true;
+            // 
+            // btnA4
+            // 
+            this.btnA4.Location = new System.Drawing.Point(337, 57);
+            this.btnA4.Name = "btnA4";
+            this.btnA4.Size = new System.Drawing.Size(66, 57);
+            this.btnA4.TabIndex = 0;
+            this.btnA4.Text = "A4";
+            this.btnA4.UseVisualStyleBackColor = true;
+            // 
+            // btnB2
+            // 
+            this.btnB2.Location = new System.Drawing.Point(139, 159);
+            this.btnB2.Name = "btnB2";
+            this.btnB2.Size = new System.Drawing.Size(66, 57);
+            this.btnB2.TabIndex = 0;
+            this.btnB2.Text = "B2";
+            this.btnB2.UseVisualStyleBackColor = true;
+            // 
+            // btnC1
+            // 
+            this.btnC1.Location = new System.Drawing.Point(43, 260);
+            this.btnC1.Name = "btnC1";
+            this.btnC1.Size = new System.Drawing.Size(66, 57);
+            this.btnC1.TabIndex = 0;
+            this.btnC1.Text = "C1";
+            this.btnC1.UseVisualStyleBackColor = true;
+            // 
+            // btnA3
+            // 
+            this.btnA3.Location = new System.Drawing.Point(233, 57);
+            this.btnA3.Name = "btnA3";
+            this.btnA3.Size = new System.Drawing.Size(66, 57);
+            this.btnA3.TabIndex = 0;
+            this.btnA3.Text = "A3";
+            this.btnA3.UseVisualStyleBackColor = true;
+            // 
+            // btnB1
+            // 
+            this.btnB1.Location = new System.Drawing.Point(43, 159);
+            this.btnB1.Name = "btnB1";
+            this.btnB1.Size = new System.Drawing.Size(66, 57);
+            this.btnB1.TabIndex = 0;
+            this.btnB1.Text = "B1";
+            this.btnB1.UseVisualStyleBackColor = true;
+            // 
+            // btnA2
+            // 
+            this.btnA2.Location = new System.Drawing.Point(139, 57);
+            this.btnA2.Name = "btnA2";
+            this.btnA2.Size = new System.Drawing.Size(66, 57);
+            this.btnA2.TabIndex = 0;
+            this.btnA2.Text = "A2";
+            this.btnA2.UseVisualStyleBackColor = true;
+            // 
+            // btnA1
+            // 
+            this.btnA1.Location = new System.Drawing.Point(43, 57);
+            this.btnA1.Name = "btnA1";
+            this.btnA1.Size = new System.Drawing.Size(66, 57);
+            this.btnA1.TabIndex = 0;
+            this.btnA1.Text = "A1";
+            this.btnA1.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalCapacity
+            // 
+            this.lblTotalCapacity.AutoSize = true;
+            this.lblTotalCapacity.Location = new System.Drawing.Point(13, 13);
+            this.lblTotalCapacity.Name = "lblTotalCapacity";
+            this.lblTotalCapacity.Size = new System.Drawing.Size(188, 25);
+            this.lblTotalCapacity.TabIndex = 1;
+            this.lblTotalCapacity.Text = "Total capacity: 12.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // lblRows
+            // 
+            this.lblRows.AutoSize = true;
+            this.lblRows.Location = new System.Drawing.Point(659, 180);
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(71, 25);
+            this.lblRows.TabIndex = 3;
+            this.lblRows.Text = "Rows:";
+            // 
+            // lblColums
+            // 
+            this.lblColums.AutoSize = true;
+            this.lblColums.Location = new System.Drawing.Point(916, 180);
+            this.lblColums.Name = "lblColums";
+            this.lblColums.Size = new System.Drawing.Size(90, 25);
+            this.lblColums.TabIndex = 3;
+            this.lblColums.Text = "Colums:";
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(659, 456);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(169, 25);
+            this.lblCustomerName.TabIndex = 3;
+            this.lblCustomerName.Text = "Customer name:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(75, 750);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(70, 25);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(664, 208);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(185, 229);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(921, 208);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(185, 229);
+            this.listBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(664, 494);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 31);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnBook
+            // 
+            this.btnBook.Location = new System.Drawing.Point(664, 574);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(132, 58);
+            this.btnBook.TabIndex = 7;
+            this.btnBook.Text = "Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(802, 574);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(132, 58);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAddToWallet
+            // 
+            this.btnAddToWallet.Location = new System.Drawing.Point(940, 574);
+            this.btnAddToWallet.Name = "btnAddToWallet";
+            this.btnAddToWallet.Size = new System.Drawing.Size(166, 58);
+            this.btnAddToWallet.TabIndex = 7;
+            this.btnAddToWallet.Text = "Add to wallet";
+            this.btnAddToWallet.UseVisualStyleBackColor = true;
+            // 
+            // btnFillAllSeats
+            // 
+            this.btnFillAllSeats.Location = new System.Drawing.Point(664, 653);
+            this.btnFillAllSeats.Name = "btnFillAllSeats";
+            this.btnFillAllSeats.Size = new System.Drawing.Size(206, 58);
+            this.btnFillAllSeats.TabIndex = 7;
+            this.btnFillAllSeats.Text = "Fill all seats";
+            this.btnFillAllSeats.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelAllBookings
+            // 
+            this.btnCancelAllBookings.Location = new System.Drawing.Point(896, 653);
+            this.btnCancelAllBookings.Name = "btnCancelAllBookings";
+            this.btnCancelAllBookings.Size = new System.Drawing.Size(210, 58);
+            this.btnCancelAllBookings.TabIndex = 7;
+            this.btnCancelAllBookings.Text = "Cancel all bookings";
+            this.btnCancelAllBookings.UseVisualStyleBackColor = true;
+            // 
+            // Venue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 797);
+            this.Controls.Add(this.btnCancelAllBookings);
+            this.Controls.Add(this.btnFillAllSeats);
+            this.Controls.Add(this.btnAddToWallet);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.lblColums);
+            this.Controls.Add(this.lblRows);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotalCapacity);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Venue";
+            this.Text = "Venue";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnC4;
+        private System.Windows.Forms.Button btnC3;
+        private System.Windows.Forms.Button btnB4;
+        private System.Windows.Forms.Button btnB3;
+        private System.Windows.Forms.Button btnC2;
+        private System.Windows.Forms.Button btnA4;
+        private System.Windows.Forms.Button btnB2;
+        private System.Windows.Forms.Button btnC1;
+        private System.Windows.Forms.Button btnA3;
+        private System.Windows.Forms.Button btnB1;
+        private System.Windows.Forms.Button btnA2;
+        private System.Windows.Forms.Button btnA1;
+        private System.Windows.Forms.Label lblTotalCapacity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRows;
+        private System.Windows.Forms.Label lblColums;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAddToWallet;
+        private System.Windows.Forms.Button btnFillAllSeats;
+        private System.Windows.Forms.Button btnCancelAllBookings;
+    }
+}
+
